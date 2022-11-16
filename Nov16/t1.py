d9c0cd3eb64a3dad2fc35bd8716762380e6cc059
Valmiki Vishwakarma <you@example.com>
2022-11-17 00:42:22 +0530

    Committing the browser launch code changes on Nov16 2022.

---
+++ Nov16/t1.py
@@ -0,0 +1,7 @@
+from selenium import webdriver;
+
+driver=webdriver.Chrome(executable_path="../Driver/chromedriver.exe")
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.get("https://www.gmail.com")
+driver.quit()
