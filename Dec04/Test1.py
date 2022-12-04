128dad6b0c7dcbacfb29169f81aa382f5b11dc91
Valmiki Vishwakarma <you@example.com>
2022-12-04 17:59:54 +0530

    Committing Test1.py

--- Dec04/Test1.py
+++ Dec04/Test1.py
@@ -1,5 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-driver=webdriver.Chrome(executable_path="../Driver/chromedriver.exe")
+driver = webdriver.Chrome(executable_path="../Driver/chromedriver.exe")
 driver.maximize_window()
-driver.get()
+url = "https://www.codementor.io/selenium-experts"
+driver.get(url)
+print(driver.find_element(By.CSS_SELECTOR,"a[title='Codementor homepage']").is_displayed())
+print(driver.find_element(By.XPATH,"//h1[contains(text(),'developer')]").text)
+driver.find_element(By.XPATH,"//span[contains(text(),'HELP')]").click()
+print(driver.find_element(By.XPATH,"//img[contains(@alt,'codementor')]").text)
+print(driver.find_element(By.XPATH,"//img[contains(@alt,'codementor')]/following-sibling::p").text)
+driver.find_element(By.CSS_SELECTOR,"input[name='name']").send_keys("Stewart Vishwa Baliyari")
