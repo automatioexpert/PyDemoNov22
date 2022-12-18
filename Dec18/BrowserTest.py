25acf754341e5d2ae8d3f92ce670907a439349af
Valmiki Vishwakarma <you@example.com>
2022-12-18 10:42:41 +0530

    Committing Python codes.

---
+++ Dec18/BrowserTest.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+
+driver=webdriver.Chrome(executable_path="../Driver/chromedriver.exe")
+driver.maximize_window()
+driver.get("https://www.google.com")
+print(driver.title)
+driver.quit()
